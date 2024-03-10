6cac42415de34d867d17aa717d007e015f949650
Ajeet Patel <patelajeet1606@gmail.com>
2024-03-10 13:48:40 +0530

    mongodb atlas connection established.

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+        console.log('MongoDB connected.. DB HOST', connectionInstance.connection.host);
+
+        // console.log('Full connection instance', connectionInstance);
+    } catch (error) {
+        console.log('MongoDB Connection FAILED', error);
+        //process is a node variable, which is a reference to current running process, so here we either throw err or exit the process with some code.
+        process.exit(1)
+    }
+}
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+});
+
+/**
+ * Approach 1: define connection in separate file, just call that here.
+ */
+connectDB();
+
+/**
+ * Approach 2: same file connection approach.
+ * 
+ * import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+ import express from 'express';
+ const app = express();
+ 
+ //we use IIFE to connect with DB, it's an async process.
+ ;( async () => {
+     try {
+         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+         app.on("error", (error) => {
+             console.log('Error', error);
+         })
+         console.log('Connection Instance', connectionInstance.connection.host);
+         app.listen(process.env.PORT, () => {
+             console.log('App is listening on port', process.env.PORT);
+         })
+     } catch (error) {
+         console.error('Error', error);
+         throw err;
+     }
+ }) ()
+
+ */
+
  
